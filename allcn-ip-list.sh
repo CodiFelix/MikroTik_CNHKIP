9ae4ce62d073cbd7176ad1f3363c648c7907de50
ITinflect-Ctrl <259126870+ITinflect-Ctrl@users.noreply.github.com>
2026-02-10 03:17:27 +0800

    Refactor allcn-ip-list.sh with updated comments and URLs
    Updated script comments and modified URLs for MikroTik address list scripts. Removed unnecessary lines and adjusted sed commands for better clarity.

--- allcn-ip-list.sh
+++ allcn-ip-list.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# 添加执行权限
 chmod +x allcn-ip-list.sh
 
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
@@ -8,7 +7,7 @@ else
   sudoCmd=""
 fi
 
-# www.github.com/itsanni，MikroTik cn address list scripts
+# www.github.com/ITinflect-Ctrl，MikroTik CN_HK address list scripts
 if [[ -f /etc/redhat-release ]]; then
   release="centos"
   systemPackage="yum"
@@ -56,10 +55,8 @@ cat delegated-apnic-latest | awk -F '|' '/CN/&&/ipv6/ {print $4 "/" $5}' | cat >
 cat delegated-apnic-latest | awk -F '|' '/HK/&&/ipv4/ {print $4 "/" 32-log($5)/log(2)}' | cat > ./Result/ipv4-hk.txt
 cat delegated-apnic-latest | awk -F '|' '/HK/&&/ipv6/ {print $4 "/" $5}' | cat > ./Result/ipv6-hk.txt
 
-#cat delegated-apnic-latest | awk -F '|' '/CN/&&/ipv6/ {print $4 "/" $5}' | cat > ipv6666.txt
 # wait for 1s
 sleep 1
-
 
 # 环境变量
 cn_ipv4_list_filename="./Result/cn_ipv4_list.rsc"
@@ -80,174 +77,50 @@ cp ./Result/ipv4-hk.txt ${cn_ipv4_hk_list_filename}
 cp ./Result/ipv4-hk.txt ${cn_ipv4_hk_route_filename}
 cp ./Result/ipv6-hk.txt ${cn_ipv6_hk_list_filename}
 
-
 # 手动添加额外需要加入的ipv4地址
 #echo "8.8.4.4/32" >> ${cn_ipv4_source_filename}
 #echo "8.8.8.8/32" >> ${cn_ipv4_source_filename}
 
-
-
-
-
-
 # IPv4-CN Firewall Address-list 地址表
-# 每行行首插入":do { add address="  list
 sed -i 's/^/:do { add address=&/g' ${cn_ipv4_list_filename}
-
-# 每行行尾插入" list=test } on-error={}"  list
 sed -i 's/$/&\ list=CN timeout=0 comment=CN } on-error={}/g' ${cn_ipv4_list_filename}
-
-# 在文件第1行前插入新行“/log info "Loading CN ipv4 address list" ”  list
 sed -i '1 i/log info "Loading CN ipv4 address list"' ${cn_ipv4_list_filename}
-
-# 在文件第5行前插入新行"/ip firewall address-list remove [/ip firewall address-list find list=cn]"  list
 sed -i '2 i/ip firewall address-list remove [find list=CN]' ${cn_ipv4_list_filename}
-
-# 在文件第6行前插入新行"/ip firewall address-list"  list
 sed -i '3 i/ip firewall address-list' ${cn_ipv4_list_filename}
 
-
-
-
 # IPv4-CN Routing Rule 地址表
-# 每行行首插入“ add dst-address=”  route
 sed -i 's/^/:do { add dst-address=&/g' ${cn_ipv4_route_filename}
-
-# 每行行尾插入“ action=lookup disabled=no table=CN comment=CN”  route
 sed -i 's/$/&\ action=lookup disabled=no table=CN comment=CN } on-error={}/g' ${cn_ipv4_route_filename}
-
-# 在文件第1行前插入新行“/log info "Loading CN ipv4 address routing" ”  route
 sed -i '1 i/log info "Loading CN ipv4 address routing"' ${cn_ipv4_route_filename}
-
-# 在文件第4行前插入新行"/routing rule remove [/routing rule find table=$list]"  route
 sed -i '2 i/routing rule remove [find table=CN]' ${cn_ipv4_route_filename}
-
-# 在文件第6行前插入新行"/routing table add name=$list fib disabled=no"  route
 sed -i '3 i/routing table add name=CN fib disabled=no' ${cn_ipv4_route_filename}
-
-# 在文件第7行前插入新行"/routing rule"  route
 sed -i '4 i/routing rule' ${cn_ipv4_route_filename}
 
-
-
-
-
 # IPv6-CN Firewall Address-list 地址表
-# 每行行首插入":do { add address="  list
 sed -i 's/^/:do { add address=&/g' ${cn_ipv6_list_filename}
-
-# 每行行尾插入" list=CN timeout=0 comment=CN } on-error={}"  list
 sed -i 's/$/&\ list=CN timeout=0 comment=CN } on-error={}/g' ${cn_ipv6_list_filename}
-
-# 在文件第1行前插入新行“/log info "Loading CN ipv6 address list" ”  list
 sed -i '1 i/log info "Loading CN ipv6 address list"' ${cn_ipv6_list_filename}
-
-# 在文件第5行前插入新行"/ipv6 firewall address-list remove [find list=CN]"  list
 sed -i '2 i/ipv6 firewall address-list remove [find list=CN]' ${cn_ipv6_list_filename}
-
-# 在文件第6行前插入新行"/ipv6 firewall address-list"  list
 sed -i '3 i/ipv6 firewall address-list' ${cn_ipv6_list_filename}
 
-
-
-
-
 # IPv4-HK Firewall Address-list 地址表
-# 每行行首插入":do { add address="  list
 sed -i 's/^/:do { add address=&/g' ${cn_ipv4_hk_list_filename}
-
-# 每行行尾插入" list=test } on-error={}"  list
 sed -i 's/$/&\ list=CN timeout=0 comment=CN } on-error={}/g' ${cn_ipv4_hk_list_filename}
-
-# 在文件第1行前插入新行“/log info "Loading CN-HK ipv4 address list" ”  list
 sed -i '1 i/log info "Loading CN-HK ipv4 address list"' ${cn_ipv4_hk_list_filename}
-
-# 在文件第5行前插入新行"/ip firewall address-list remove [/ip firewall address-list find list=cn]"  list
 sed -i '2 i/ip firewall address-list remove [find list=CN]' ${cn_ipv4_hk_list_filename}
-
-# 在文件第6行前插入新行"/ip firewall address-list"  list
 sed -i '3 i/ip firewall address-list' ${cn_ipv4_hk_list_filename}
 
-
-
-
-
 # IPv4-HK Routing Rule 地址表
-# 每行行首插入“ add dst-address=”  route
 sed -i 's/^/:do { add dst-address=&/g' ${cn_ipv4_hk_route_filename}
-
-# 每行行尾插入“ action=lookup disabled=no table=CN comment=CN”  route
 sed -i 's/$/&\ action=lookup disabled=no table=CN comment=CN } on-error={}/g' ${cn_ipv4_hk_route_filename}
-
-# 在文件第1行前插入新行“/log info "Loading CN-HK ipv4 address routing" ”  route
 sed -i '1 i/log info "Loading CN-HK ipv4 address routing"' ${cn_ipv4_hk_route_filename}
-
-# 在文件第4行前插入新行"/routing rule remove [/routing rule find table=$list]"  route
 sed -i '2 i/routing rule remove [find table=CN]' ${cn_ipv4_hk_route_filename}
-
-# 在文件第6行前插入新行"/routing table add name=$list fib disabled=no"  route
 sed -i '3 i/routing table add name=CN fib disabled=no' ${cn_ipv4_hk_route_filename}
-
-# 在文件第7行前插入新行"/routing rule"  route
 sed -i '4 i/routing rule' ${cn_ipv4_hk_route_filename}
 
-
-
-
-
 # IPv6-HK Firewall Address-list 地址表
-# 每行行首插入":do { add address="  list
 sed -i 's/^/:do { add address=&/g' ${cn_ipv6_hk_list_filename}
-
-# 每行行尾插入" list=test } on-error={}"  list
 sed -i 's/$/&\ list=CN timeout=0 comment=CN } on-error={}/g' ${cn_ipv6_hk_list_filename}
-
-# 在文件第1行前插入新行“/log info "Loading CN-HK ipv6 address list" ”  list
 sed -i '1 i/log info "Loading CN-HK ipv6 address list"' ${cn_ipv6_hk_list_filename}
-
-# 在文件第5行前插入新行"/ipv6 firewall address-list remove [find list=CN]"  list
 sed -i '2 i/ipv6 firewall address-list remove [find list=CN]' ${cn_ipv6_hk_list_filename}
-
-# 在文件第6行前插入新行"/ipv6 firewall address-list"  list
 sed -i '3 i/ipv6 firewall address-list' ${cn_ipv6_hk_list_filename}
-
-
-
-
-
-# 删除list文件尾的换行（可选）
-#tail -n 1 ${cn_ipv4_list_filename} | tr -d '\n' >> gfwlist_lastline_tmp
-#sed -i '$d' ${cn_ipv4_list_filename}
-#sed -i '$r gfwlist_lastline_tmp' ${cn_ipv4_list_filename}
-#rm -f gfwlist_lastline_tmp
-
-# 删除route文件尾的换行（可选）
-#tail -n 1 ${cn_ipv4_route_filename} | tr -d '\n' >> gfwlist_lastline_tmp
-#sed -i '$d' ${cn_ipv4_route_filename}
-#sed -i '$r gfwlist_lastline_tmp' ${cn_ipv4_route_filename}
-#rm -f gfwlist_lastline_tmp
-
-# 删除list文件尾的换行（可选）
-#tail -n 1 ${cn_ipv6_list_filename} | tr -d '\n' >> gfwlist_lastline_tmp
-#sed -i '$d' ${cn_ipv6_list_filename}
-#sed -i '$r gfwlist_lastline_tmp' ${cn_ipv6_list_filename}
-#rm -f gfwlist_lastline_tmp
-
-
-# 删除list文件尾的换行（可选）
-#tail -n 1 ${cn_ipv4_hk_list_filename} | tr -d '\n' >> gfwlist_lastline_tmp
-#sed -i '$d' ${cn_ipv4_hk_list_filename}
-#sed -i '$r gfwlist_lastline_tmp' ${cn_ipv4_hk_list_filename}
-#rm -f gfwlist_lastline_tmp
-
-# 删除route文件尾的换行（可选）
-#tail -n 1 ${cn_ipv4_hk_route_filename} | tr -d '\n' >> gfwlist_lastline_tmp
-#sed -i '$d' ${cn_ipv4_hk_route_filename}
-#sed -i '$r gfwlist_lastline_tmp' ${cn_ipv4_hk_route_filename}
-#rm -f gfwlist_lastline_tmp
-
-# 删除list文件尾的换行（可选）
-#tail -n 1 ${cn_ipv6_hk_list_filename} | tr -d '\n' >> gfwlist_lastline_tmp
-#sed -i '$d' ${cn_ipv6_hk_list_filename}
-#sed -i '$r gfwlist_lastline_tmp' ${cn_ipv6_hk_list_filename}
-#rm -f 
